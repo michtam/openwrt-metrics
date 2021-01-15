a4391689f8203fd07c55505c0d3037ffc6f6ac65
Marcin <marcin.michta@gmail.com>
2021-01-15 19:55:33 +0100

    Update push-metrics.sh

--- push-metrics.sh
+++ push-metrics.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# awk -F'^:' 'NR>2{print $1}' /proc/net/dev
+
 INFLUXDB=${1}
 HOSTNAME=${2}
 shift; shift
@@ -13,15 +15,25 @@ echo interfaces: ${INTERFACES}
 while true; do
   for interface in $INTERFACES ; do
     interface_stats=$(grep $interface /proc/net/dev | sed s/.*://)
+
     recived_1=$(echo ${interface_stats} | awk '{print $1}')
     sent_1=$(echo ${interface_stats} | awk '{print $9}')
     sleep 1
+
     timestamp=$(date +%s)
     interface_stats=$(grep $interface /proc/net/dev | sed s/.*://)
+
     recived_2=$(echo ${interface_stats} | awk '{print $1}')
     sent_2=$(echo ${interface_stats} | awk '{print $9}')
-    in_speed=$(( ( ${recived_2} - ${recived_1} ) / 1024 ))
-    out_speed=$(( ( ${sent_2} - ${sent_1} ) / 1024))
-    curl -s -XPOST "${INFLUXDB}/write?db=${database}&precision=s" --data-binary "bandwidth,host=${HOSTNAME},interface=${interface} inbound=${in_speed},outbound=${out_speed} ${timestamp}"
+
+    bytes_recv=$(( ( ${recived_2} - ${recived_1} ) ))
+    bytes_sent=$(( ( ${sent_2} - ${sent_1} ) ))
+
+    #logger -t stats "[${timestamp}]:${interface} inbound=${bytes_recv} outbound=${bytes_sent}"
+    #echo "[${timestamp}]:${interface} inbound=${in_speed} outbound=${out_speed}"
+
+    curl -s -XPOST "${INFLUXDB}/write?db=${database}&precision=s" \
+      --data-binary "net,host=${HOSTNAME},interface=${interface} bytes_recv=${bytes_recv},bytes_sent=${bytes_sent} ${timestamp}"
+    sleep 5
   done;
 done;
