e11bec85729ada84c910ef3833acbace59cff322
michtu <marcin.michta@gmail.com>
2021-01-16 17:22:23 +0100

    grab-all-metrics.sh improvements

--- grab-all-metrics.sh
+++ grab-all-metrics.sh
@@ -4,7 +4,11 @@ HOSTNAME="gateway.local"
 INFLUXDB_URL="http://influxdb.local:8086"
 DB_NAME="network"
 
+logger -t grab-all-metrics "Starting... 30 seconds delay..."
 # waiting for all interfaces especially wlan0/1
 sleep 30
+
+ifaces=$(awk -F':' 'NR>2{print $1}' /proc/net/dev)
+logger -t grab-all-metrics "Found interfaces: ${ifaces}"
 
 awk -F':' 'NR>2{print $1}' /proc/net/dev | xargs -n 1 -P 12 ${SCRIPT} ${INFLUXDB_URL} ${HOSTNAME} ${DB_NAME}
